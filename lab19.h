862c7429526019ebeecf53e3310bfc139cea6ad7
TanlnWZaa007 <153242404+TanlnWZaa007@users.noreply.github.com>
2026-02-17 17:23:16 +0700

    Update lab19.h

--- lab19.h
+++ lab19.h
@@ -2,6 +2,7 @@
 #include<string>
 #include<ctime>
 #include<cstdlib>
+#include<vector>
 #include<iomanip>
 
 using namespace std;
@@ -13,16 +14,16 @@ class Unit{
 		int hpmax;
 		int atk;
 		int def;
-		bool guard_on;		
+		bool guard_on;
 	public:			
 		void create(string);
 		void showStatus();
 		void newTurn();
 		int attack(Unit &);
 		int beAttacked(int);
-		int heal();	
+		int heal();
 		void guard();
-		bool isDead();	
+		bool isDead();
 };
 
 void Unit::create(string t){ 
@@ -46,81 +47,99 @@ void Unit::create(string t){
 
 void Unit::showStatus(){
 	if(type == "Hero"){
-		cout << "---------------------------------------\n"; 
-		cout << name << "\n"; 
-		cout << "HP: " << hp << "/" << hpmax << "\tATK: "<< atk << "\t\tDEF: "<< def;		
+		cout << "---------------------------------------\n";
+		cout << name << "\n";
+		cout << "HP: " << hp << "/" << hpmax << "\t\tATK: "<< atk << "\t\tDEF: "<< def;		
 		cout << "\n---------------------------------------\n";
 	}	
 	else if(type == "Monster"){
-		cout << "\t\t\t\t---------------------------------------\n"; 
-		cout << "\t\t\t\t" << name << "\n"; 
+		cout << "\t\t\t\t---------------------------------------\n";
+		cout << "\t\t\t\t" << name << "\n";
 		cout << "\t\t\t\tHP: " << hp << "\t\tATK: "<< atk << "\t\tDEF: "<< def;
 		cout << "\n\t\t\t\t---------------------------------------\n";
 	}
 }
 
 void Unit::newTurn(){
-	guard_on = false;
+	guard_on = false; 
 }
 
+void Unit::guard(){
+	guard_on = true;
+}
 
+int Unit::heal(){
+	int h = rand()%21 + 10;
+	if(hp + h > hpmax) h = hpmax - hp;
+	hp += h;
+	return h;
+}
 
-/////////////////////////////////////////////////////////////////////////////////////
-//Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
-/////////////////////////////////////////////////////////////////////////////////////
+int Unit::beAttacked(int opp_atk){
+	int dmg = opp_atk - def;
+	if(guard_on) dmg = dmg/3;
+	if(dmg < 0) dmg = 0;
+	hp -= dmg;
+	return dmg;
+}
 
+int Unit::attack(Unit &target){
+	return target.beAttacked(atk);
+}
 
+bool Unit::isDead(){
+	if(hp <= 0) return true;
+	else return false;
+}
 
-void drawScene(char p_action,int p,char m_action,int m){
+void drawScene(char p_action, int p, char m_action, int m){
 	cout << "                                                       \n";
 	if(p_action == 'A'){
-	cout << "                                       "<< -p <<"\n";
+		cout << "                                       " << -p << "\n";
 	}else{
-	cout << "                                                       \n";	
+		cout << "                                                       \n";	
 	}	
-	cout << "                                *               *      \n";
-	cout << "                                **  *********  **      \n";
-	cout << "                                ****         ****      \n";
+	cout << "                                * * \n";
+	cout << "                                ** ********* ** \n";
+	cout << "                                **** **** \n";
 	if(m_action == 'A'){
-	cout << "                 " << setw(5) << -m << "           *** **   ** ***       Attack!\n";
+		cout << "                 - " << m << "           *** ** ** *** Guard!\n";
 	}else if(m_action == 'G'){
-	cout << "                                 *** **   ** ***       Guard!\n";
+		cout << "                                *** ** ** *** Guard!\n";
 	}else{
-	cout << "                                 *** **   ** ***       \n";	
+		cout << "                                *** ** ** *** \n";
 	}
-	cout << "                                  ** **   ** **        \n";
-	cout << "                   ***             *         *         \n";
+	cout << "                                 ** ** ** ** \n";
+	cout << "                   *** * * \n";
 	if(p_action == 'A'){
-	cout << "        Attack!    ***  *           *********          \n";		
+		cout << "        Attack!    *** * ********* \n";
 	}else if(p_action == 'H'){
-	cout << "      Heal! +" << setw(2) << p << "    ***  *           *********          \n";
+		cout << "      Heal! +" << p << "    *** * ********* \n";
 	}else if(p_action == 'G'){
-	cout << "         Guard!    ***  *           *********          \n";
+		cout << "         Guard!    *** * ********* \n";
 	}else{
-	cout << "                   ***  *           *********          \n";	
+		cout << "                   *** * ********* \n";
 	}
-	cout << "                    *  *       ***  *  *  *            \n";
-	cout << "                  *****           **   *   *           \n";
-	cout << "                    *                  *    ***        \n";
-	cout << "                   * *               **        *       \n";
-	cout << "                  *   *                                \n";
+	cout << "                    * * *** * * * \n";
+	cout << "                  ***** ** * * \n";
+	cout << "                    * * *** \n";
+	cout << "                   * * ** * \n";
+	cout << "                  * * \n";
 	cout << "                                                       \n";
 };
-
 
 void playerWin(){	
 	cout << "*******************************************************\n";
-	for(int i = 0; i < 3; i++) cout << "*                                                     *\n";
-	cout << "*                   YOU WIN!!!                        *\n";
-	for(int i = 0; i < 3; i++) cout << "*                                                     *\n";
+	for(int i = 0; i < 3; i++) cout << "* *\n";
+	cout << "* YOU WIN!!!                        *\n";
+	for(int i = 0; i < 3; i++) cout << "* *\n";
 	cout << "*******************************************************\n";
 };
 
-
 void playerLose(){
 	cout << "*******************************************************\n";
-	cout << "*                                                     *\n";
-	cout << "*                   YOU LOSE!!!                       *\n";
-	cout << "*                                                     *\n";
+	cout << "* *\n";
+	cout << "* YOU LOSE!!!                       *\n";
+	cout << "* *\n";
 	cout << "*******************************************************\n";
 };
